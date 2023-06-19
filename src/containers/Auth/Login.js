033521e7a1a5433546649db14526fd198bbf57f2
Van Quynh <phamvanquynhk44@gmail.com>
2023-06-19 11:48:24 +0700

    css form

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -15,8 +15,35 @@ class Login extends Component {
     render() {
 
         return (
-            <div id="loginform">
-          </div>
+            <div className='login-bakground'>
+                <div className='login-container'>
+                    <div className='login-content row'>
+                        <div className='col-12 text-login'>Login</div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Username:</label>
+                            <input type='text' className='form-control' placeholder='Enter your username'/>
+                        </div>
+                        <div className='col-12 form-group login-input'>
+                            <label>Password:</label>
+                            <input type='password' className='form-control' placeholder='Enter your password'/>
+                        </div>
+                        <div className='col-12 '>
+                            <button className='btn-login'>Login</button>
+                        </div>  
+                        <div className='col-12'>
+                            <span className='forgot-password'>Forgot your password?</span>
+                        </div>
+                        <div className='col-12 text-center mt-3'>
+                            <span className='text-other-login'>Or Login with:</span>
+                        </div>
+                        <div className='col-12 social-login'>
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
+                        </div>
+                           
+                    </div>
+                </div>
+            </div>
  
         )
     }
