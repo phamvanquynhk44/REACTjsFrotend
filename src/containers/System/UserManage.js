cada1c022adeb932d2bdbc2679ce85474ad1c162
Van Quynh <phamvanquynhk44@gmail.com>
2023-06-21 18:10:47 +0700

    Display User Data to View (READ) using CRUD 2nd time

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import {getAllUsers} from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state ={
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
+    // life cycle
+    // run component:
+    // 1. Run construct -> init state
+    // 2. Did mount (set State)
+    // 3. Render
 
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode ===0){
+            this.setState({
+                arrUsers: response.users
+            })
+        }
     }
 
 
     render() {
+        let arrUsers=this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center'>Manage users with quynh</div>
+                <div className='users-table mt-4 mx-1'>
+                <table id="customers">
+                    <tr>
+                        <th>Email</th>
+                        <th>Fullname</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </tr>
+                    
+                        {arrUsers && arrUsers.map((item, index) =>{
+                            return(
+                                <tr>
+                                <td>{item.email}</td>
+                                <td>{item.fullname}</td>
+                                <td>{item.address}</td>
+                                <td>
+                                    <button className='btn-edit'><i class="fas fa-pencil-alt"></i></button>
+                                    <button className='btn-delete'><i class="fas fa-trash-alt"></i></button>
+                                </td>
+                        </tr>
+                            )
+                        })
+                        }
+
+                    </table>
+                </div>
+            </div>
         );
     }
 
