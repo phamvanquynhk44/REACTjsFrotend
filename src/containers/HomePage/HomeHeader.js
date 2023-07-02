be5a139d28343507109e5a7d7045c8ed0af5a6b0
Van Quynh <phamvanquynhk44@gmail.com>
2023-07-02 19:39:58 +0700

    Using Redux To Switch Languages In React App

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,11 +3,18 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.svg';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils';
+
+import { changeLanguageApp } from '../../store/actions';
 
 class HomeHeader extends Component {
 
+    changeLanguage=(language)=>{
+        this.props.changeLanguageAppRedux(language)
+    }
+
     render() {
-        console.log('check pros: ', this.props)
+        let language= this.props.language;
         const { isLoggedIn } = this.props;
         let linkToRedirect = isLoggedIn ? '/system/user-manage' : '/home';
 
@@ -39,8 +46,8 @@ class HomeHeader extends Component {
                    </div>
                    <div className='right-content'>
                         <div className='support'><i class="far fa-question-circle"></i> <FormattedMessage id="homeheader.support"/></div>
-                        <div className='language-vi'>VN</div>
-                        <div className='language-en'>EN</div>
+                        <div className={ language === LANGUAGES.VI ? 'language-vi active' : 'language-vi' }><span onClick={()=>this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                        <div className={ language === LANGUAGES.EN ? 'language-en active' : 'language-en' }><span onClick={()=>this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                    </div>
                 </div>
             </div>
@@ -97,6 +104,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
